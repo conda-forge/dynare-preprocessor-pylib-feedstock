eec8586cd3aef1b5d4632653b8efe3e1f0a30097
Pablo Winant <pablo.winant@gmail.com>
2025-07-22 01:36:12 +0200

    Enable library build.

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ meson setup --prefix=$PREFIX --bindir=$PREFIX/bin --libdir=$PREFIX/lib --include
     --buildtype=release build_preproc \
     -Dcpp_args="-w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11"  \
     -Dcpp_link_args="-w  -Wno-enum-constexpr-conversion -I${PREFIX}/include/pybind11" \
+    -Dbuild_library="enabled"
 
 meson compile -C build_preproc
 meson install -C build_preproc #--destdir="../
