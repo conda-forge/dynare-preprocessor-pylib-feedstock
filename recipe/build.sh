2d2fbadcba3c099a448fe74e8a03ff0b6d82889a
Pablo Winant <pablo.winant@gmail.com>
2025-09-09 21:11:28 +0200

    Distinct options on macos and linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,14 +6,20 @@ set -x
 export BOOST_ROOT=$PREFIX
 if [ "$(uname)" == "Darwin" ]; then
   # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-  CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-fi
+meson setup --prefix=$PREFIX --bindir=$PREFIX/bin --libdir=$PREFIX/lib --includedir=$PREFIX/include \
+    --buildtype=release build_preproc \
+    -Dcpp_args="-w  -Wno-enum-constexpr-conversion  -_LIBCPP_DISABLE_AVAILABILITY"  \
+    -Dcpp_link_args="-w  -Wno-enum-constexpr-conversion" \
+    -Dbuild_library="enabled"
 
+else
 meson setup --prefix=$PREFIX --bindir=$PREFIX/bin --libdir=$PREFIX/lib --includedir=$PREFIX/include \
     --buildtype=release build_preproc \
     -Dcpp_args="-w  -Wno-enum-constexpr-conversion"  \
     -Dcpp_link_args="-w  -Wno-enum-constexpr-conversion" \
     -Dbuild_library="enabled"
+fi
+
 
 meson compile -C build_preproc
 meson install -C build_preproc #--destdir="../
