81c8295598096c1bf4e8761387a1b6ea943c6781
Pablo Winant <pablo.winant@gmail.com>
2025-09-09 20:49:38 +0200

    Not quite sure, what I'm doing.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,13 +4,10 @@ set -e
 set -x
 
 export BOOST_ROOT=$PREFIX
-
 if [ "$(uname)" == "Darwin" ]; then
   # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
-
-CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
 meson setup --prefix=$PREFIX --bindir=$PREFIX/bin --libdir=$PREFIX/lib --includedir=$PREFIX/include \
     --buildtype=release build_preproc \
